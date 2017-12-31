f83bd7dc9b5e447c1f83a628c0465590f98a291e
Virandry <virandry@gmail.com>
2017-12-31 22:18:00 +0800

    Add files via upload

---
+++ src/main/java/io/virandry/vire/VireApplication.java
@@ -0,0 +1,12 @@
+package io.virandry.vire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VireApplication.class, args);
+	}
+}
---
+++ src/test/java/io/virandry/vire/VireApplicationTests.java
@@ -0,0 +1,16 @@
+package io.virandry.vire;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VireApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
